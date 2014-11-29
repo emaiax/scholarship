730adb5f366b87d46c5aa506a384aa4339ee8f9f
Eduardo Maia <eduvimaia@gmail.com>
2014-11-29 18:00:25 -0200

    adicionado campo de tipo de usuário em seu model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129183452) do
+ActiveRecord::Schema.define(version: 20141129194725) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20141129183452) do
     t.string   "encrypted_password", default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
