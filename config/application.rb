420ea824745308971fccd0417714e0b09b5c2b50
Eduardo Maia <eduvimaia@gmail.com>
2014-11-29 17:35:11 -0200

    configuração do projeto pra carregar as validações customizadas de CNPJ

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ module Scholarship
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.autoload_paths += %W[#{config.root}/lib]
+    config.autoload_paths += %W[#{config.root}/lib/validators]
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
