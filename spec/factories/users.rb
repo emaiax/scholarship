67f432809b2bd0c9a492c69be1ead3181d003a85
Eduardo Maia <eduvimaia@gmail.com>
2014-11-30 16:02:39 -0200

    corrigida factory de usuários pra usar o faker

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,19 @@
 FactoryGirl.define do
   factory :user do
-    
-  end
+    sequence :email do |n|
+      "email-#{n}@scholarship.com"
+    end
 
+    name     { Faker::Name.name }
+    password { Faker::Internet.password }
+
+    factory :secretary do
+      role :secretary
+      association :school, strategy: :build
+    end
+
+    factory :admin do
+      role :admin
+    end
+  end
 end
